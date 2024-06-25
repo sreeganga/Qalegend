39e93db861edb68eadcd95a81041c72591ff6420
user <user@DESKTOP-6FJKV3E>
2024-06-25 14:48:55 +0530

    last

--- qalegend/src/test/java/Automationcore/Base.java
+++ qalegend/src/test/java/Automationcore/Base.java
@@ -1,7 +1,10 @@
 package Automationcore;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -16,11 +19,24 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
 import Utility.WaitUtility;
+import constants.Constants;
 
 public class Base {
 	protected WebDriver driver;
-	public  void intialiseBrowser(String browser)
+	public Properties prop;
+	public FileInputStream fs;
+	public  void intialiseBrowser(String browser) throws IOException
 	{
+		try 
+		{
+		prop = new Properties();
+		fs = new FileInputStream(Constants.CONFIGFILE);
+		prop.load(fs);
+	} 
+		catch (FileNotFoundException e) 
+		{
+		e.printStackTrace();
+	}
 		
 		if(browser.equals("Chrome"))
 		{
@@ -44,9 +60,9 @@ public class Base {
 		
 		
 	}
-	@BeforeMethod
+	@BeforeMethod(alwaysRun = true)
 	@Parameters({"browser","baseurl"})
-	  public void setup(String browsername , String url) 
+	  public void setup(String browsername , String url) throws IOException 
 	  { 
 		
 		  intialiseBrowser(browsername); 
@@ -54,7 +70,7 @@ public class Base {
 		  WaitUtility.waitUsingImplicitWait(driver);
 		  
 	  }
-@AfterMethod
+@AfterMethod(alwaysRun = true)
    public void closeBrowser(ITestResult result) throws IOException
    {
 	if(result.getStatus()==ITestResult.FAILURE)
